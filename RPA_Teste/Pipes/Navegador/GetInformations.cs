c7f376a54b3914d94682e96711f3cb3ea14810be
Vinicius Dias <vinicius.rodrigues@fiskaldigital.com.br>
2024-04-25 16:53:10 -0300

    adicionando metodo de envio de mensagem via telegram

--- RPA_Teste/Pipes/Navegador/GetInformations.cs
+++ RPA_Teste/Pipes/Navegador/GetInformations.cs
@@ -9,17 +9,28 @@ namespace RPA_Teste.Pipes.Navegador
     {
         public static IReadOnlyCollection<IWebElement> Challenge(ChromeDriver driver)
         {
-            driver.Navigate().GoToUrl("https://www.fundsexplorer.com.br/funds/htmx11");
             Thread.Sleep(1000);
+            driver.Navigate().GoToUrl("https://www.fundsexplorer.com.br/funds/htmx11");
+            Indicadores indicadores = new Indicadores();
 
-            IReadOnlyCollection<IWebElement> elementosIndicadores = driver.FindElements(By.XPath("//*[@id=\"indicators\"]/div/p[2]/b"));
-            
-            foreach (IWebElement element in elementosIndicadores) 
-            {
+            var elementosIndicadores = driver.FindElements(By.XPath("//*[@id='indicators']/div/p[2]/b"));
+            IList<IWebElement> listaElementos = elementosIndicadores.ToList();
 
-                Console.WriteLine("elemento: " + element.Text);
+            indicadores.LiquidezMediaDiaria = elementosIndicadores[0].Text;
+            indicadores.UltimoRendimento = elementosIndicadores[1].Text;
+            indicadores.DividendYield = elementosIndicadores[2].Text;
+            indicadores.PatrimonioLiquido = elementosIndicadores[3].Text;
+            indicadores.ValorPatrimonial = elementosIndicadores[4].Text;
+            indicadores.RentabilidadeNoMes = elementosIndicadores[5].Text;
+            indicadores.PVP = elementosIndicadores[6].Text;
+            indicadores.UltimoDividendo = elementosIndicadores[7].Text;
+            indicadores.DividendYieldUltimoDividendo = elementosIndicadores[8].Text;
+            indicadores.DivPorAcao = elementosIndicadores[9].Text;
 
-            }
+
+            Telegram.TelegramApi.SendMessageAsync(indicadores.LiquidezMediaDiaria).Wait();
+
+            Console.WriteLine(indicadores.LiquidezMediaDiaria);
 
             return elementosIndicadores;
         }
---
+++ RPA_Teste/Telegram/TelegramApi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace RPA_Teste.Telegram
+{
+    internal class TelegramApi
+    {
+        private static TelegramBotClient _botClient = new TelegramBotClient("7138305614:AAF2KBe6uMKdxZxHG4aeYbSZc2n5A8hzs_Y");
+        private static long chatId = -1002090888464; // Id do grupo RPA Tora
+
+        public static async Task SendMessageAsync(string message)
+        {
+            try
+            {
+                await _botClient.SendTextMessageAsync(chatId, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending message: {ex.Message}");
+            }
+        }
+        public static async Task SendImageAsync(string imagePath, string caption)
+        {
+            try
+            {
+                if (!System.IO.File.Exists(imagePath))
+                {
+                    Console.WriteLine($"Error: Image file '{imagePath}' does not exist.");
+                }
+
+                await using (Stream stream = System.IO.File.OpenRead(imagePath))
+                {
+                    await _botClient.SendPhotoAsync(chatId, photo: InputFile.FromStream(stream, Path.GetFileName(imagePath)), caption: caption);
+                    Console.WriteLine($"Image '{Path.GetFileName(imagePath)}' sent successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending image: {ex.Message}");
+            }
+        }
+    }
+}
